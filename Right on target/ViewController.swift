0cb82eed17da78a352776cf95e4bca21c257f2a8
Амелькин Владимир <amelkin@vbankcenter.ru>
2021-11-20 19:18:58 +0300

    Initial Commit

---
+++ Right on target/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Right on target
+//
+//  Created by Владимир Амелькин on 20.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
